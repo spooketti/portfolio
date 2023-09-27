4139bd731cf69e2592e834446dde3a4981ef0af5
spooketti <jonliu168@gmail.com>
2023-09-26 17:25:24 -0700

    organization

---
+++ assets/js/home.js
@@ -0,0 +1,34 @@
+window.addEventListener("scroll",function(e)
+{
+    topDist = this.scrollY
+    document.getElementById("ParalaxName").style.transform = `translate(0px,-${(topDist*(1/100))}px)`
+    document.getElementById("ParalaxDesc").style.transform = `translate(0px,-${(topDist*(.25))}px)`
+    for(let i=1;i<=3;i++)
+    {
+        document.getElementById("par"+i.toString()).style.transform = `translate(0px,-${(topDist*(i/5))}px)`
+    }
+    document.getElementById("par4".toString()).style.transform = `translate(0px,-${(topDist*(0.3))}px)`
+})
+
+function spinBody()
+{
+    if(spinning)
+    {
+        return;
+    }
+    spinning = true
+    document.body.style.transform = "rotate(1turn)"
+    document.body.style.transition = "all 4s ease 0s"
+    setTimeout(() => {
+        document.body.style.transform = null
+        document.body.style.transition = null
+        spinning = false
+     }, "4000") 
+
+}
+
+function mineSweepToggle()
+{
+document.getElementById("minesweeperWrapper").style.display = "flex"
+document.getElementById("msiframe").style.display = "block"
+}
--- assets/js/page.js
+++ assets/js/page.js
@@ -4,10 +4,11 @@ let navbarVis = true
 let navBar = document.getElementById("header")
 let spinning = false
 let reducedMotion = false
-if(localStorage.reducedMotion)
+if(localStorage.getItem("reducedMotion") == "true")
 {
-    reducedMotion = localStorage.reducedMotion
+    reducedMotion = true
 }
+
 localStorage.reducedMotion = reducedMotion
 redMotDict = 
 {
@@ -22,25 +23,6 @@ function toggleMot()
     localStorage.reducedMotion = reducedMotion
     redMotButton.innerText = "Reduced Motion: " + redMotDict[reducedMotion]
 }
-
-window.addEventListener("scroll",function(e)
-{
-    topDist = this.scrollY
-    document.getElementById("ParalaxName").style.transform = `translate(0px,-${(topDist*(1/100))}px)`
-    document.getElementById("ParalaxDesc").style.transform = `translate(0px,-${(topDist*(.25))}px)`
-    for(let i=1;i<=3;i++)
-    {
-        document.getElementById("par"+i.toString()).style.transform = `translate(0px,-${(topDist*(i/5))}px)`
-    }
-    document.getElementById("par4".toString()).style.transform = `translate(0px,-${(topDist*(0.3))}px)`
-})
-let slideClassArray = [
-    "slideOpacity"
-]
-
-
-//let entString = entry.target.classList.toString().split(" ").filter((word)=> !slideClassArray.includes(word)) //gets string's classlist as a string, split by spaces into arrya, filter every word to make sure it doesn't exist in the array
-
 const observer = new IntersectionObserver((entries) => 
 {
     
@@ -88,32 +70,10 @@ document.addEventListener("scroll", function(){
       navBar.style.transform = "translateY(-100%)"
       
    } else if (ypos < scrollbuffer) {
-    console.log("up")
+    //console.log("up")
       navbarVis = true
       navBar.style.transform = "translateY(0)"
    } 
    scrollbuffer = ypos <= 0 ? 0 : ypos; 
 });
 
-function spinBody()
-{
-    if(spinning)
-    {
-        return;
-    }
-    spinning = true
-    document.body.style.transform = "rotate(1turn)"
-    document.body.style.transition = "all 4s ease 0s"
-    setTimeout(() => {
-        document.body.style.transform = null
-        document.body.style.transition = null
-        spinning = false
-     }, "4000") 
-
-}
-
-function mineSweepToggle()
-{
-document.getElementById("minesweeperWrapper").style.display = "flex"
-document.getElementById("msiframe").style.display = "block"
-}
